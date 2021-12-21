79022310c545316269b25c5a22735e77386966de
Aarish <46606156+aarish-rajput@users.noreply.github.com>
2021-12-21 11:54:25 +0500

    initial

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,12 +2,13 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        auth: {
-            user: process.env.SMTP_EMAIL,
-            pass: process.env.SMTP_PASSWORD
-        }
+        service: 'gmail',
+        // host: process.env.SMTP_HOST,
+        // port: process.env.SMTP_PORT,
+         auth: {
+             user: process.env.REAL_EMAIL,
+             pass: process.env.REAL_PASSWORD
+         }
     });
 
     const message = {
